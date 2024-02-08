cdd7d3c7c3dc121a8dcdc288ab91505ea6a6a54b
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-02-08 10:21:23 +0100

    a new begining, added react-vis, might change later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { HorizontalGridLines, LineSeries, VerticalGridLines, XAxis, XYPlot, YAxis } from 'react-vis';
 
 function App() {
+  const data = [
+    {x: 0, y: 8},
+    {x: 1, y: 5},
+    {x: 2, y: 4},
+    {x: 3, y: 9},
+    {x: 4, y: 1},
+    {x: 5, y: 7},
+    {x: 6, y: 6},
+    {x: 7, y: 3},
+    {x: 8, y: 2},
+    {x: 9, y: 0}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <XYPlot width={300} height={300}>
+      <LineSeries data={data}/>
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <YAxis />
+      <XAxis />
+    </XYPlot>  
     </div>
   );
 }
