bde42bc6fb6e854687507b9792bbcf29289e25c9
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-02-08 20:07:12 +0100

    added .csv files to public/ and figured out a way to import them as a json object (thanks random person on stackoverflow who asked just the right question and the one who answered it as well)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { HorizontalGridLines, LineSeries, VerticalGridLines, XAxis, XYPlot, YAxis } from 'react-vis';
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+
 
 function App() {
-  const data = [
-    {x: 0, y: 8},
-    {x: 1, y: 5},
-    {x: 2, y: 4},
-    {x: 3, y: 9},
-    {x: 4, y: 1},
-    {x: 5, y: 7},
-    {x: 6, y: 6},
-    {x: 7, y: 3},
-    {x: 8, y: 2},
-    {x: 9, y: 0}
-  ];
+
+  async function getData(src) {
+    const data = Papa.parse(await fetchCsv(src), {header: true, dynamicTyping: true}).data;
+    console.log(data);
+    return data;
+  }
+
+  async function fetchCsv(src) {
+    const response = await fetch(src);
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = await decoder.decode(result.value);
+    console.log('csv', csv);
+    return csv;
+  }
+
+  const srcNor = getData('emissions_no.csv');
+  const srcQat = getData('emissions_qa.csv');
+
   return (
     <div className="App">
-    <XYPlot width={300} height={300}>
-      <LineSeries data={data}/>
-      <VerticalGridLines />
-      <HorizontalGridLines />
-      <YAxis />
-      <XAxis />
-    </XYPlot>  
+      <canvas id='chart'></canvas>
     </div>
   );
 }
